f36c4514acddfd096d5ce7f414e3bec2f5848e24
joeyabou <joeyta_ac@hotmail.com>
2022-10-16 21:33:00 -0400

    Spring Boot initializr set up

---
+++ src/main/java/com/BuildingManager/BuildingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.BuildingManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BuildingManager/BuildingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BuildingManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
